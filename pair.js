c7ea0f791164861a91296f0cc6803568525cdfd8
ꜱɢ ᴅᴀʀᴋ 神 <godwithusmustgr8@gmail.com>
2026-07-04 16:20:33 +0100

    Refactor Dark-Bot to DarkBot and update function names

--- pair.js
+++ pair.js
@@ -5,8 +5,9 @@ const express = require('express');
 const fs = require('fs');
 let router = express.Router();
 const pino = require('pino');
+
 const {
-    default: Dark-Bot,
+    default: DarkBot,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
@@ -21,41 +22,58 @@ function removeFile(FilePath) {
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-    
-    async function DARK-BOT_PAIR_CODE() {
+
+    async function DARK_BOT_PAIR_CODE() {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+
         try {
-            let Pair_Code_By_Dark-Bot = Dark-Bot({
+            let Pair_Code_By_DarkBot = DarkBot({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
+                    keys: makeCacheableSignalKeyStore(
+                        state.keys,
+                        pino({ level: 'fatal' }).child({ level: 'fatal' })
+                    ),
                 },
                 printQRInTerminal: false,
                 logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
                 browser: Browsers.macOS('Chrome')
             });
 
-            if (!Pair_Code_By_Dark-Bot.authState.creds.registered) {
+            if (!Pair_Code_By_DarkBot.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const code = await Pair_Code_By_Dark-Bot.requestPairingCode(num);
+
+                const code = await Pair_Code_By_DarkBot.requestPairingCode(num);
+
                 if (!res.headersSent) {
                     await res.send({ code });
                 }
             }
 
-            Pair_Code_By_Dark-Boy.ev.on('creds.update', saveCreds);
-            Pair_Code_By_Dark-Bot.ev.on('connection.update', async (s) => {
+            Pair_Code_By_DarkBot.ev.on('creds.update', saveCreds);
+
+            Pair_Code_By_DarkBot.ev.on('connection.update', async (s) => {
                 const { connection, lastDisconnect } = s;
+
                 if (connection === 'open') {
                     await delay(5000);
-                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                    await delay(800);
-                    let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Dark-Bot.sendMessage(Pair_Code_By_Dark-Bot.user.id, { text: 'DARK-BOT~' + b64data });
 
-                    let Dark-Bot_TEXT = `
-        
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+
+                    await delay(800);
+
+                    let b64data = Buffer.from(data).toString('base64');
+
+                    let session = await Pair_Code_By_DarkBot.sendMessage(
+                        Pair_Code_By_DarkBot.user.id,
+                        {
+                            text: 'DARK-BOT~' + b64data
+                        }
+                    );
+
+                    let DarkBot_TEXT = `
+
 ╔════════════════════◇
 ║『 SESSION CONNECTED』
 ║ ✨ DARK-BOT 🔷
@@ -68,7 +86,7 @@ router.get('/', async (req, res) => {
 ╔════════════════════◇
 ║『 YOU'VE CHOSEN Dark-Bot 』
 ║ -Set the session ID in Heroku:
-║ - SESSION_ID: 
+║ - SESSION_ID:
 ╚════════════════════╝
 ╔════════════════════◇
 ║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
@@ -87,26 +105,49 @@ router.get('/', async (req, res) => {
 Don't Forget To Give Star⭐ To My Repo
 ______________________________`;
 
-                    await Pair_Code_By_Dark-Bot.sendMessage(Pair_Code_By_Dark-Boy.user.id, { text: Toxic_MD_TEXT }, { quoted: session });
+                    await Pair_Code_By_DarkBot.sendMessage(
+                        Pair_Code_By_DarkBot.user.id,
+                        {
+                            text: DarkBot_TEXT
+                        },
+                        {
+                            quoted: session
+                        }
+                    );
 
                     await delay(100);
-                    await Pair_Code_By_Dark-Bot.ws.close();
+
+                    await Pair_Code_By_DarkBot.ws.close();
+
                     return await removeFile('./temp/' + id);
-                } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+
+                } else if (
+                    connection === 'close' &&
+                    lastDisconnect &&
+                    lastDisconnect.error &&
+                    lastDisconnect.error.output.statusCode != 401
+                ) {
+
                     await delay(10000);
-                    Dark-Bot_PAIR_CODE();
+
+                    DARK_BOT_PAIR_CODE();
                 }
             });
+
         } catch (err) {
             console.log('Service restarted');
+
             await removeFile('./temp/' + id);
+
             if (!res.headersSent) {
-                await res.send({ code: 'Service Currently Unavailable' });
+                await res.send({
+                    code: 'Service Currently Unavailable'
+                });
             }
         }
     }
-    
-    return await Dark-Bot_PAIR_CODE();
+
+    return await DARK_BOT_PAIR_CODE();
 });
 
 module.exports = router;
